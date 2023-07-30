f591d38897f5b0714b51ab09fac9afd2229cc7e6
gnaneshwar <gnaneshwar718@gmail.com>
2023-07-30 14:11:18 +0530

    intial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://media2.giphy.com/media/RuRd67S6IfDEI/giphy.gif?cid=ecf05e4709a7bo9s4pa3jjy8158qysrszxdof5sgznizxkze&ep=v1_gifs_search&rid=giphy.gif&ct=g';
+
+export const categories = [
+  { name: 'home', icon: <HomeIcon />, },
+  { name: 'linus tech tips', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy series hollywood', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'sciencefacts', icon: <DeveloperModeIcon />, },
+  { name: 'React', icon: <CodeIcon />, },
+  { name: 'morning-music', icon: <MusicNoteIcon /> },
+  { name: 'coding', icon:  <CodeIcon />, },
+  { name: 'music', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'valorant', icon: <SportsEsportsIcon />, },
+  { name: 'tv', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'netflix', icon: <CodeIcon />, },
+  { name: 'technology', icon: <CodeIcon />, },
+  { name: 'coding interview', icon: <CodeIcon />, },
+  { name: 'astrophysics', icon: <CodeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
